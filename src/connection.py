c7ca06191d7e5e276eea81ca6a7120a1218df79e
Ciwy <c.walter@afk-dev.de>
2023-05-09 09:26:53 +0200

    add: socket implementation for twitch chat

---
+++ src/connection.py
@@ -0,0 +1,42 @@
+import socket
+
+class Connection:
+    """ socket connection wrapper to connect to twitch
+        irc and eventsub
+
+    """
+
+    BUFF_SIZE = 1024
+
+
+    def __init__(self, addr):
+        self.addr = addr
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connected = False     
+
+
+    def connect(self):
+        self.sock.connect(self.addr)
+        self.connected = True
+
+
+    def send(self, msg):
+        data = bytes(msg + "\n", 'ASCII')
+
+        self.sock.send(data)
+
+
+    def receive(self, buffer=''):
+        msg = self.sock.recv(self.BUFF_SIZE)
+        buffer += msg.decode('UTF-8')
+        if len(msg) == self.BUFF_SIZE:
+            return self.receive(buffer)
+            
+        return buffer
+
+
+    def pong(self, data):
+        msg = f'PONG {data}'
+        self.send(msg)
+
+
