bdd6559c95bd211dd7bd6aba74af8c6fa3408644
Qtian <c.walter@afk-dev.de>
2023-05-12 08:34:45 +0200

    add: plugin for github

--- src/plugins/__init__.py
+++ src/plugins/__init__.py
@@ -1,4 +1,5 @@
 from src.plugins.users import UsersPlugin
 from src.plugins.messages import MessagesPlugin
+from src.plugins.github import GithubPlugin
 
-PLUGINS = [UsersPlugin, MessagesPlugin]
+PLUGINS = [UsersPlugin, MessagesPlugin, GithubPlugin]
