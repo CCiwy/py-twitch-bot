4efed599e1225d7bfdbb17c184d4fdfceac5bbdb
Ciwy <c.walter@afk-dev.de>
2023-05-12 18:05:20 +0200

    add: server routing for static files

--- src/server.py
+++ src/server.py
@@ -1,22 +1,38 @@
 # Import Third-Party
 from bottle import Bottle, Route
+from bottle import static_file
 
+# Import Home-Grown
 from src.utils.logger import get_logger
-
 logger = get_logger('Server')
 
 class Server(Bottle):
     """ Wrapper around Bottle to enable adding routes programmaticly"""
 
-    def __init__(self, app):
-        self.application_context = app
+    def __init__(self, app, *args, **kwargs):
+        self.app = app
         super().__init__()
+        
+        static_files = kwargs.get('static_files', False)
+        if static_files:
+            self.init_static_files(static_files)
+
+
+
+    def init_static_files(self, static_files):
+        self.static_files = static_files
+        self.add_route('/static/<filename>', 'GET', self.get_app_static)
+    
+
+    def get_app_static(self, filename):
+        return static_file(filename, root=self.static_files)
+
 
     def get_app_url(self, endpoint, *args):
         endpoint = endpoint + '/' if not endpoint.endswith('/') else endpoint
         combined_url = endpoint + '/'.join([a for a in args])
-        self.application_context.logger.debug(f'combined_url: {combined_url} <= {args}')
         return super().get_url(combined_url)
+
 
     def add_route(self, rule, http_method, callback):
         route = Route(self, rule, http_method, callback)
