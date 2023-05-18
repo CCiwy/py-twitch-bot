ee6f108929cb04cc0599e552d49f708249f2f650
Ciwy <c.walter@afk-dev.de>
2023-05-18 12:52:44 +0200

    rework: plugins.base BasePlugin storage of commands

--- src/plugins/base.py
+++ src/plugins/base.py
@@ -5,7 +5,6 @@ from bottle import request
 
 class BotPlugin:
     """ plugin base class. might use as protocol """
-    _save_attrs = []
     _name = False
     instances = {}
 
@@ -14,6 +13,8 @@ class BotPlugin:
         self.app = app
         self.handler = app.chat
         self.instances[self.name] = self
+        self._commands = {}
+
 
     @property
     def name(self):
@@ -22,17 +23,65 @@ class BotPlugin:
             _name = cls_name.lower().replace('plugin', '')
             self._name = _name
         return self._name       
+
+
+    def add_command(self, ident: str, *args, **kwargs) -> None:
+        cmd_cls = StaticCommand
+
+        if kwargs.get('callback', False):
+            cmd_cls = CallbackCommand
+
+        # initialize command
+        cmd = cmd_cls(self, ident, *args, **kwargs)
+        self._commands[ident] = cmd
         
+    
 
     def get_save(self):
         return {attr_name : getattr(self, attr_name, False) for attr_name in self._save_attrs}
+    def register_command(self, command: str):
+        self.handler.register_command(self, command)
+
+
+
+
+    def get_command(self, ident):
+        """ this is called from within our application """
+        return self._commands.get(ident, False)
+
+    
+    def get_command_names(self) -> set[str]:
+        _commands = set([c.command for c in self._commands.values() if c.is_active])
+        return _commands
+   
+
+    def execute_command(self, cmd_name, *args, **kwargs):
+        # todo: cmd_name does not equal ident
+        command = self._commands[cmd_name]
+        message = command.execute(*args, **kwargs)
         
 
+        if message:
+            self.handler.send_message(message)
 
     def load(self, save_game):
         for attr, value in save_game.items():
             if attr in self._save_attrs:
                 setattr(self, attr, value)
+
+    def update_commands(self, _updates):
+        for ident, new_data in _updates.items():
+            if ident in self._commands:
+                self._update_command(ident, new_data)
+
+
+    def _update_command(self, ident, new_data):
+        command = self.get_command(ident)
+        if new_data != command.get_data():
+            # todo: ident does not equal command
+            command.set_data(new_data)
+
+
         
 
 
